a223d57407ec22b5c3ce64a7345133bfc071d8b6
Surendra Vasamsetti <leelasurendravasamsetti@gmail.com>
2024-09-15 17:26:53 +0530

    first commit

---
+++ LandingPage.js
@@ -0,0 +1,16 @@
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
